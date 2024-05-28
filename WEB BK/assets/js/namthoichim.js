0e319710f46930a3b7474f13ac1fe219abb96d89
Huyhavertz29 <dhuy85628@gmail.com>
2024-05-28 22:57:57 +0700

    dump commit

---
+++ WEB BK/assets/js/namthoichim.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.btn-order').click(function(){
+        $('.modal').css('display', 'flex').show(1000);
+    })
+    $('.btn').click(function(){
+        $('.modal').css('display', 'none');
+    })
+})
