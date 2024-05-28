124d9b262130d3d693286a6ef58b1bdcdfbbabe2
Huyhavertz29 <dhuy85628@gmail.com>
2024-05-28 23:07:36 +0700

    last commit

--- WEB BK/assets/js/namthoichim.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.btn-order').click(function(){
-        $('.modal').css('display', 'flex').show(1000);
-    })
-    $('.btn').click(function(){
-        $('.modal').css('display', 'none');
-    })
-})
